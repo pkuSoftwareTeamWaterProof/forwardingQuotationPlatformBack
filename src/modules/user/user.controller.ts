ddc0bd31b295b7f9207f139c08203a12d4a38209
WaterKing <WaterKing210821@outlook.com>
2023-11-13 20:35:18 +0800

    feat: create and search firm and user
    有如下bug：
    
    1. 添加操作没有判断是否有重名
    2. 搜索用户操作没有对不存在的情况进行处理

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { CreateUserDTO } from './dto/createUser.dto';
-import { User } from './entity/user.entity';
+import { CreateCustomerDTO, CreateForwarderDTO, CreateAdministratorDTO, CreateUserDTO, CreateFirmDTO } from './user.dto';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 import { ApiTags } from '@nestjs/swagger';
 
@@ -11,13 +11,39 @@ export class UserController {
 
   @Post()
   async createUser(@Body() createUserDTO: CreateUserDTO): Promise<void> {
-    await this.userService.createUser(createUserDTO);
+    switch(createUserDTO.type){
+      case 'forwarder':
+        await this.userService.createForwarder((createUserDTO as CreateForwarderDTO));
+        break;
+      case 'customer':
+        await this.userService.createCustomer((createUserDTO as CreateCustomerDTO));
+        break;
+      case 'administrator':
+        await this.userService.createAdministrator((createUserDTO as CreateAdministratorDTO));
+        break;
+      default:
+        throw TypeError("Unknown user role: "+createUserDTO.type)
+    }
     return;
   }
 
+  
   @Get(':userId')
-  getUserById(@Param() userId: string): Promise<User> {
+  getUserById(@Param('userId') userId: string): Promise<User> {
     const user = this.userService.getUserById(userId);
+    console.log(user);
     return user;
+  }
+}
+
+@ApiTags("firm")
+@Controller("firm")
+export class FirmController{
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createFirm(@Body() createFirmDTO:CreateFirmDTO): Promise<void>{
+    await this.userService.createFirm(createFirmDTO);
+    return;
   }
 }
---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,91 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  ManyToOne,
+  OneToMany
+} from 'typeorm';
+
+@Entity()
+export class Firm{
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column()
+  description:string|null;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    name: 'created_at',
+  })
+  createdAt: Date | undefined;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    name: 'updated_at',
+  })
+  updatedAt: Date | undefined;
+
+  @OneToMany(type => Forwarder, forwarder => forwarder.firm)
+  employees: Forwarder[];
+}
+
+export enum UserRole{
+  CUSTOMER = "customer",
+  FORWARDER = "forwarder",
+  ADMINISTRATOR = "administrator"
+}
+
+export abstract class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    name: 'created_at',
+  })
+  createdAt: Date | undefined;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    name: 'updated_at',
+  })
+  updatedAt: Date | undefined;
+}
+
+@Entity()
+export class Customer extends User {
+  @Column({nullable:true})
+  telephone: string
+
+  @Column({nullable:true})
+  email: string
+}
+
+@Entity()
+export class Forwarder extends User {
+  @Column({nullable:true})
+  telephone: string;
+
+  @Column({nullable:true})
+  email: string;
+
+  @ManyToOne(type=>Firm, firm=>firm.employees)
+  firm:Firm;
+}
+
+@Entity()
+export class Administrator extends User {
+
+}
