2cbd6216da12438d634470d95ca5f25b243f9d07
WaterKing201030 <947488670@qq.com>
2023-12-11 19:46:47 +0800

    delete jwtconstant

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-import { jwtConstants } from './constants';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
