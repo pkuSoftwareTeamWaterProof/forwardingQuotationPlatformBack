0fba88df00d6409f688c97c609320c298adcce69
WaterKing201030 <947488670@qq.com>
2023-12-21 10:44:08 +0800

    fix import dependece

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -17,5 +17,6 @@ import { UserModule } from '../user/user.module';
   ],
   controllers: [OrderController],
   providers: [OrderService, SheetService, AnswerService],
+  exports: [OrderService]
 })
 export class OrderModule {}
